6808c40ae4cf7c4baf62c3cee194544bdf1384e0
Joshi Vallejo <josvalle@Joshis-MacBook-Pro.local>
2018-10-24 08:52:58 -0700

    heroku add

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-
+const bodyparser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
