aec2068831a9ba21ca6fa0eccd226a9822bb73a4
Joshi Vallejo <josvalle@Joshis-MacBook-Pro.local>
2018-10-24 09:03:43 -0700

    added routes for email and created cat-fact request

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const emailer = require('./routes/emailer');
+
+app.use(bodyparser.json());
+
+app.use('/emailer', emailer);
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
